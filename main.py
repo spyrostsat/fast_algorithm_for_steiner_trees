bf531cac692d67de15ec44600216bd7f93cdcac5
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2024-02-26 02:00:57 +0200

    final commit

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ import networkx as nx
 import random
 
 
-for i in range(6, 7):
-    
-    G = nx.gnm_random_graph(n=i, m=i+5)
+START = 6
+END = 13
+EDGES_INCREASE = 3
+
+for i in range(START, END):
+    G = nx.gnm_random_graph(n=i, m=i+EDGES_INCREASE)
     
     for u, v in G.edges:
         G[u][v]["weight"] = random.randint(1, 50)
-    G:nx.Graph = nx.relabel_nodes(G, {node: f"V{node+1}" for node in range(i)})  # vertices will be named from "V1" to "Vn" where n is the number of vertices
-    S:IVertices = random.sample(list(G.nodes), round(0.7*i))
     
-    Ts = solve_steiner_tree_problem(G, S)
-    print(calculate_total_weight_of_graph(Ts))
+    G:nx.Graph = nx.relabel_nodes(G, {node: f"V{node+1}" for node in range(i)})  # vertices will be named from "V1" to "Vn"
+    S:IVertices = random.sample(list(G.nodes), round(0.7*i))  # randomly select 70% of the vertices to be the Steiner vertices
     
+    try:
+        Ts = solve_steiner_tree_problem(G=G, S=S, plot_charts=True)
+        print(f"Nodes: {i} | Total weight of Steiner Tree: {calculate_total_weight_of_graph(Ts)}")
+    except nx.exception.NetworkXNoPath:
+        print(f"Nodes: {i} | No available Steiner Tree. The graph is not connected.")
--- src/utils.py
+++ src/utils.py
@@ -10,10 +10,11 @@ def calculate_total_weight_of_graph(G: nx.Graph) -> float:
     return sum([G.get_edge_data(u, v)["weight"] for u, v in G.edges])
 
 
-def plot_graph(G: nx.Graph) -> None:
+def plot_graph(G: nx.Graph, title: str) -> None:
     '''
     This function plots a graph using networkx and matplotlib libraries.
     '''
+    plt.title(title)
     pos = nx.spring_layout(G)
     nx.draw(G, pos, with_labels=True, node_color="lightblue", edge_color="gray", node_size=500, font_size=15, font_weight="bold")
     nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, "weight"))
@@ -43,9 +44,10 @@ def complete_subgraph_from_graph_with_shortest_paths(G: nx.Graph, vertices: IVer
 
 def build_subgraph_from_graph_with_minimum_spanning_tree(G: nx.Graph, minimum_spanning_tree: nx.Graph) -> nx.Graph:
     '''
-    This function builds a subgraph from a given graph using a minimum spanning tree. The subgraph will contain all vertices of the minimum spanning tree.
-    For each edge in the minimum spanning tree, we should find the shortest path between these two vertices from the original graph.
-    Then all additional vertices found in the shortest paths should be added to the subgraph to connect the vertices of the minimum spanning tree.
+    This function builds a subgraph from a given graph using a minimum spanning tree. The subgraph will contain all vertices of the
+    minimum spanning tree. For each edge in the minimum spanning tree, we should find the shortest path between these two vertices
+    from the original graph. Then all additional vertices found in the shortest paths should be added to the subgraph to connect the
+    vertices of the minimum spanning tree.
     '''
     subgraph = nx.Graph()
     
@@ -58,39 +60,41 @@ def build_subgraph_from_graph_with_minimum_spanning_tree(G: nx.Graph, minimum_sp
     return subgraph
 
 
-def remove_nodes_leaves_from_minimum_spanning_tree(minimum_spanning_tree: nx.Graph, desired_leaves: IVertices) -> nx.Graph:
+def remove_nodes_leaves_from_minimum_spanning_tree(minimum_spanning_tree: nx.Graph, allowed_leaves: IVertices) -> nx.Graph:
     '''
-    This function takes as input a minimum spanning tree and a list of desired leaves.
-    It removes from the minimum spanning tree all leaves that are not in the desired leaves list.
-    Then, from the resulting tree, it again removes all leaves that are not in the desired leaves list.
-    The process repeats until the resulting tree has only the desired leaves.
+    This function takes as input a minimum spanning tree and a list of allowed leaves (Steiner vertices).
+    It removes from the minimum spanning tree all leaves that are not in the allowed leaves list.
+    The process repeats until all leaves in the resulting tree are in the allowed leaves list.
     '''    
     while True:
         leaves = [node for node in minimum_spanning_tree.nodes if minimum_spanning_tree.degree(node) == 1]
-        leaves_to_remove = [leaf for leaf in leaves if leaf not in desired_leaves]
-        print(leaves, leaves_to_remove)
-        if not leaves_to_remove:
-            break
-        minimum_spanning_tree.remove_nodes_from(leaves_to_remove)
+        leaves_to_remove = [leaf for leaf in leaves if leaf not in allowed_leaves]
+
+        if not leaves_to_remove: break
         
+        minimum_spanning_tree.remove_nodes_from(leaves_to_remove)
+    
     return minimum_spanning_tree
 
 
-def solve_steiner_tree_problem(G: nx.Graph, S: IVertices) -> nx.Graph:
+def solve_steiner_tree_problem(G: nx.Graph, S: IVertices, plot_charts: bool = False) -> nx.Graph:
     '''
     This function solves the Steiner Tree Problem for a given graph and a list of Steiner vertices.
     '''
     G1 = complete_subgraph_from_graph_with_shortest_paths(G, S)
     T1:nx.Graph = nx.minimum_spanning_tree(G1)
-    Gs:nx.Graph = build_subgraph_from_graph_with_minimum_spanning_tree(G=G, minimum_spanning_tree=T1)
+    
+    Gs:nx.Graph = build_subgraph_from_graph_with_minimum_spanning_tree(G, T1)
     Ts:nx.Graph = nx.minimum_spanning_tree(Gs)
-    Th:nx.Graph = remove_nodes_leaves_from_minimum_spanning_tree(minimum_spanning_tree=Ts, desired_leaves=S)
+    
+    Th:nx.Graph = remove_nodes_leaves_from_minimum_spanning_tree(Ts, S)
 
-    plot_graph(G)
-    plot_graph(G1)
-    plot_graph(T1)
-    plot_graph(Gs)
-    plot_graph(Ts)
-    plot_graph(Th)
+    if plot_charts:
+        plot_graph(G, f"Original Graph G containing {len(G.nodes)} vertices and {len(G.edges)} edges")
+        plot_graph(G1, f"Complete Subgraph G1 containing {len(G1.nodes)} vertices and {len(G1.edges)} edges (Shortest Paths)")
+        plot_graph(T1, "Minimum Spanning Tree T1 of the Complete Subgraph")
+        plot_graph(Gs, "Subgraph Gs containing the Minimum Spanning Tree Edges and Shortest Paths")
+        plot_graph(Ts, "Minimum Spanning Tree Ts of the Subgraph Gs")
+        plot_graph(Th, "Final Steiner Tree Th")
     
     return Th
